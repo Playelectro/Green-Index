45cfc753b6baaa6422c96cf3c213b5cf8d622821
Playelectro <nebo_GOD@yahoo.com>
2023-11-12 15:06:58 +0200

    removed restx

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 from flask import Flask
-from flask_restx import Api, Resource
 from config import DevConfig
 import folium as foll
 
 
 app = Flask(__name__)
-api = Api(app, doc="/docs")	
 
 app.config.from_object(DevConfig)
 
-
-# Api example - TODO : Remove
-@api.route('/hello')
-class HelloResource(Resource):
-    def get(self):
-        return {"message":"Hello World"}
 
 @app.route('/home')
 def home():
