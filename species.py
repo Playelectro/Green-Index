7decb4db2c9b3d68012ea4c95d0b6bbe133197c4
danbsaucvnush <danbusuioc82@gmail.com>
2023-11-17 19:57:34 +0200

    ye

--- species.py
+++ species.py
@@ -2,5 +2,8 @@
 from flask import (Flask, render_template, redirect, request,
                    send_from_directory, )
 
+sp_list = None
+
 def species_entry(list):
+    sp_list=list
     return render_template('species.html', list=list)
---
+++ static/js/lightbox.js
@@ -0,0 +1,8 @@
+function onClick(e){
+    var parent = e.parentElement;
+    console.log(parent.id);
+  /* for (const child of parent.children()) {
+        console.log(child.tagName);
+      }*/
+    
+}
