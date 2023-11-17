7b115101f0a7ecb44c14f9901893b7e44f15b167
SebyChi <chialdaseby@gmail.com>
2023-11-17 17:26:22 +0200

    dwaasdaw

--- species.py
+++ species.py
@@ -2,5 +2,5 @@
 from flask import (Flask, render_template, redirect, request,
                    send_from_directory, )
 
-def species_entry():
-    return render_template('species.html')
+def species_entry(list):
+    return render_template('species.html', list=list)
